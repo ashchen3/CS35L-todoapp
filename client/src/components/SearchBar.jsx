ddd7d00c62ce90523ed26e403d4c4fbf8b6d2bc0
Wei Han <chuaweihan0101@gmail.com>
2024-03-10 01:00:39 -0800

    Refactored SearchBar.jsx
    - Resolved key error warning using unique keys.
    - Enable search input to persist after user clicks off the searchbar.

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,132 +1,120 @@
-import { TextField, Autocomplete, Typography, Box } from '@mui/material'
+import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 
 function SearchBar({ tasklistData }) {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <Autocomplete
-      disablePortal
-      fullWidth
-      display="flex"
-      id="tasklist-and-task-search"
-      options={ reformatTasklistData(tasklistData) }
-      getOptionLabel={ (option) => option.title }
-      isOptionEqualToValue={ (option, value) => option.title.toLowerCase().includes(value.title.toLowerCase()) }
-      groupBy={ (option) => option.type }
-
-      // Redirects to list view upon clicking on any element
-      onChange={(event, option) => {
+    /** Handles when a search item is clicked; brings user to list view. */
+    const handleSearchClicked = (event, option) => {
         var tasklistId;
-        if(option.type == 'Tasks') tasklistId = option.tasklist.id;
-        else tasklistId = option.id;
-        navigate('/list', { state: { selectedTasklistId: tasklistId } });
-      }}
-
-      renderInput={ renderSearchBarInput }
-      renderOption={ renderSearchBarOption }
-    />
-   );
-}
-
-function reformatTasklistData(tasklistData){
-
-  if (!tasklistData) return [];
-
-  const tasklists = JSON.parse(JSON.stringify(tasklistData));  // Note that this cloning process converts Dates to strings
-  tasklists.forEach((tasklist) => {
-    delete tasklist.tasks;
-    tasklist.type = 'Tasklists';
-  });
-
-  const tasks = [].concat(
-    ...tasklistData.map((tasklist) => 
-      tasklist.tasks.map((task) => {
-        task['tasklist'] = {
-          id: tasklist.id,
-          title: tasklist.title
+        if (option.type == "Tasks") {
+            tasklistId = option.tasklist.id;
+        } else {
+            tasklistId = option.id;
         }
-        return task;
-      })
-    )
-  ).map((task) => ({ ...task, type: 'Tasks' }));
+        navigate("/list", {
+            state: { selectedTasklistId: tasklistId },
+        });
+    };
 
-  return [].concat(tasklists, tasks);
-}
-
-function renderSearchBarInput(params, tasklistData){
-  return (<TextField {...params}
-    label="Search your lists..."
-    sx={{
-     margin: '10px auto',
-    }}
-  />)
-}
-
-function renderSearchBarOption(props, option){
-  const elements = [
-    <Typography noWrap
-      sx={{
-        width: 250,
-        pr: '1em',
-      }}
-    >
-      {option.title}
-    </Typography>
-  ];
-
-  if(option.type == 'Tasks'){
-    elements.push(
-      <Typography 
-        noWrap
-        color="#9e9e9e"
-        sx={{
-          width: 250,
-        }}
-      >
-        In <i>{option.tasklist.title}</i>
-      </Typography>
+    return (
+        <Autocomplete
+            disablePortal
+            fullWidth
+            clearOnBlur={false}
+            display="flex"
+            id="tasklist-and-task-search"
+            options={reformatTasklistData(tasklistData)}
+            getOptionLabel={(option) => option.title}
+            isOptionEqualToValue={(option, value) =>
+                option.title.toLowerCase().includes(value.title.toLowerCase())
+            }
+            groupBy={(option) => option.type}
+            // Redirects to list view upon clicking on any element
+            onChange={(e, option) => handleSearchClicked(e, option)}
+            renderInput={renderSearchBarInput}
+            renderOption={renderSearchBarOption}
+        />
     );
-  }
-  
-  return (<li {...props} key={option.id}>
-    <Box key={"Box "+option.id}
-      style={{
-        display: "flex"
-      }}
-    >
-      {elements}
-    </Box>
-  </li>);
+}
+
+/**
+ * Formats the searchbar data; combines tasklists and tasks to form a list.
+ */
+function reformatTasklistData(tasklistData) {
+    if (!tasklistData) return [];
+
+    const tasklists = JSON.parse(JSON.stringify(tasklistData)); // Note that this cloning process converts Dates to strings
+    tasklists.forEach((tasklist) => {
+        delete tasklist.tasks;
+        tasklist.type = "Tasklists";
+    });
+
+    const tasks = []
+        .concat(
+            ...tasklistData.map((tasklist) =>
+                tasklist.tasks.map((task) => {
+                    task["tasklist"] = {
+                        id: tasklist.id,
+                        title: tasklist.title,
+                    };
+                    return task;
+                })
+            )
+        )
+        .map((task) => ({ ...task, type: "Tasks" }));
+
+    return [...tasklists, ...tasks];
+}
+
+function renderSearchBarInput(params) {
+    return (
+        <TextField
+            {...params}
+            label="Search your lists..."
+            sx={{
+                margin: "10px auto",
+            }}
+        />
+    );
+}
+
+function renderSearchBarOption(props, option) {
+    return (
+        <Box
+            component="li"
+            style={{
+                display: "flex",
+            }}
+            {...props}
+            key={`${option.type}-${option.id}`}
+        >
+            <Typography
+                noWrap
+                sx={{
+                    width: 250,
+                    pr: "1em",
+                }}
+            >
+                {option.title}
+            </Typography>
+
+            {option.type === "Tasks" && (
+                <Typography
+                    noWrap
+                    color="#9e9e9e"
+                    sx={{
+                        width: 250,
+                    }}
+                >
+                    In <i>{option.tasklist.title}</i>
+                </Typography>
+            )}
+        </Box>
+    );
 }
 
 export default SearchBar;
-
-/* DEPRECATED SEARCHBAR
-    <TextField
-      label={"Search your lists..."}
-      margin="normal"
-      fullWidth={true}
-      onInput={(e) => {
-        setSearchQuery(e.target.value);
-      }}
-      onSubmit={(e) => e.preventDefault}
-    />
-*/
-
-/* DEPRECATED SEARCH BAR FILTERING FUNCTION
-function searchBarFilter(query, data){
-  if (!query) return null;
-
-  var searchResults = {};
-  searchResults.tasklists = data.filter((tasklist) => tasklist.title.toLowerCase().includes(query.toLowerCase()));
-
-  searchResults.tasks = [].concat(...data.map((tasklist) => 
-    tasklist.tasks.filter((task) =>
-      task.title.toLowerCase().includes(query.toLowerCase())
-    )
-  ));
-
-  return searchResults;
-}
-*/
